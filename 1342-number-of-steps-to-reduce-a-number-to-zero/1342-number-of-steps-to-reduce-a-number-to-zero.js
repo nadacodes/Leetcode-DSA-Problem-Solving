609c3ed0bfa0c97aebeb6114b6f1be58032f4730
Nada Chraf <70055041+nadacodes@users.noreply.github.com>
2022-09-11 14:12:44 +0200

    Time: 111 ms (14.29%), Space: 41.8 MB (81.02%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -3,21 +3,16 @@
  * @return {number}
  */
 var numberOfSteps = function(num) {
-  
-    if(num==0){
-       return 0;
-    }
-    
+
     let count=0; 
     
     while(true){
         
-    num%2==0? num=num/2:num--;
-    count++;   
-        
     if(num==0){
        return count;
-      }
+    }
         
+    num%2==0? num=num/2:num--;
+    count++;  
     }
 };
