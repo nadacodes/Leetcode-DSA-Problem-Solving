d1d360c2f9a9468ed25af92079ec98b002a778ff
Nada Chraf <70055041+nadacodes@users.noreply.github.com>
2022-09-11 19:56:03 +0200

    Time: 119 ms (55.88%), Space: 42.4 MB (75.00%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -7,9 +7,9 @@ var twoSum = function(nums, target) {
     var result=[];
     for(var i=0; i < nums.length; i++)
         {
-             for(var j=1;j<nums.length; j++)
+             for(var j=1;j<nums.length && i!=j; j++)
                  {
-                       if(i!=j && nums[i]+nums[j]==target)
+                       if(nums[i]+nums[j]==target)
                            {
                                  result.push(i);
                                  result.push(j);
