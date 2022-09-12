47f9bf0a6ea847a4539e8167716dda273ac9641a
Nada Chraf <70055041+nadacodes@users.noreply.github.com>
2022-09-12 21:49:58 +0200

    Time: 291 ms (44.80%), Space: 51.1 MB (58.60%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -4,13 +4,8 @@
  */
 var isPalindrome = function(x) {
     var str= x.toString().split("");
- 
-    if(str.length%2!=0)
-       {
-           str.splice(str.length/2 - 0.5, 1);
-       }
-   
-    
+    str.length%2!=0? str.splice(str.length/2 - 0.5, 1): str=str;
+                                
     for(var i=0;i<str.length;i++)
         {
             if(str[i]!=str[str.length-i-1])
